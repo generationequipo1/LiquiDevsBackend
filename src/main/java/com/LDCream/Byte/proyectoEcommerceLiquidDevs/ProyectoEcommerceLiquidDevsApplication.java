1fda97f95c973990bc9dd617f97a5a8fe9b3a692
luisvillada <otrodeluisvillada@gmail.com>
2026-01-13 16:23:40 -0500

    Se sube paquete base con spring boot de byte&cream

---
+++ src/main/java/com/LDCream/Byte/proyectoEcommerceLiquidDevs/ProyectoEcommerceLiquidDevsApplication.java
@@ -0,0 +1,13 @@
+package com.LDCream.Byte.proyectoEcommerceLiquidDevs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoEcommerceLiquidDevsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoEcommerceLiquidDevsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LDCream/Byte/proyectoEcommerceLiquidDevs/ProyectoEcommerceLiquidDevsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LDCream.Byte.proyectoEcommerceLiquidDevs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoEcommerceLiquidDevsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
