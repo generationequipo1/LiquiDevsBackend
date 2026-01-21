380343d77f9f893d4102d95d864e65a6456c4d2b
Lorena <pontongeidy@gmail.com>
2026-01-21 00:14:37 -0500

    ultima actualizaciones de conexion a backend a frontend

--- src/main/java/com/LDCream/Byte/proyectoEcommerceLiquidDevs/controller/UsuarioController.java
+++ src/main/java/com/LDCream/Byte/proyectoEcommerceLiquidDevs/controller/UsuarioController.java
@@ -2,12 +2,14 @@ package com.LDCream.Byte.proyectoEcommerceLiquidDevs.controller;
 
 import com.LDCream.Byte.proyectoEcommerceLiquidDevs.model.Usuario;
 import com.LDCream.Byte.proyectoEcommerceLiquidDevs.service.IusuarioService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios")
+@CrossOrigin(origins = "*")
 public class UsuarioController {
 private final IusuarioService usuarioService;
 
@@ -16,20 +18,28 @@ private final IusuarioService usuarioService;
     }
 
     @GetMapping
-    public List<Usuario> usuarios(){
-        return usuarioService.buscarTodos();
+    public List<Usuario> usuarios(){return usuarioService.buscarTodos();
     }
-    @GetMapping("/{id}/")
+    @GetMapping("/{id}")
     public Usuario obtenerUsuario(@PathVariable Long id){
         return usuarioService.buscarPorId(id).orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
     }
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/crear")
     public Usuario crearUsuario(@RequestBody Usuario usuario){
 
          return usuarioService.guardar(usuario);
     }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void eliminarUsuario(@PathVariable Long id) {
+        usuarioService.eliminarPorId(id);
+    }
 
-
+    @PutMapping("/{id}")
+    public void editarUsuario(@PathVariable Long id, @RequestBody Usuario usuarioActualizado) {
+        usuarioService.editarUsuario(id, usuarioActualizado);
+    }
 
 
 }
